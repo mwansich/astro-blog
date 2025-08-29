c709dfd8b5c1399ebd462e79fcbd69979165b5da
residuum-01 <78220994+residuum-01@users.noreply.github.com>
2025-08-29 12:30:48 +0200

    added darkmode (not styled correctly yet) and finished tutorial

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const greeting = useState(randomMessage());
+
+  return (
+    <div>
+      <h2>{greeting}! Thank you for visiting!</h2>
+    </div>
+  );
+}
