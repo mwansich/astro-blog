2bb78239abcbe7725744fd64cb85460636fc7611
mwansich <70520740+mwansich@users.noreply.github.com>
2025-12-05 20:30:36 +0100

    rss feed improvements

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,18 +7,25 @@ const parser = new MarkdownIt();
 export async function GET(context) {
   const posts = await getCollection('blog');
   return rss({
-    title: 'My Astro Blog',
-    description: 'Latest posts from my Astro blog',
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+    title: 'My CS50 Final Project Blog',
+    description: 'Latest posts from my CS50 Final Project blog',
     site: context.site,
+    // Add Atom namespace to the <rss> tag
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+    },
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate.toISOString().split('T')[0],
       description: post.data.description,
       link: `/posts/${post.id}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      content: sanitizeHtml(parser.render(post.body).replace(/href="\//g, `href="${context.site}`), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat([])
       }),
     })),
-    customData: `<language>en-us</language>`,
+    // Add atom:link inside the <channel> element
+    customData: `<language>en-us</language>
+      <atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
   });
 }
