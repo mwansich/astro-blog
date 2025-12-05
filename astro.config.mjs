7edb00634cb2996556e902120f6ea2dbdc57123f
mwansich <70520740+mwansich@users.noreply.github.com>
2025-12-05 22:46:16 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
   site: "https://deluxe-frangipane-d091d8.netlify.app/"
 });
